174395e9538550fff85d322f2b336cf4854ec033
David <learnitmyway@users.noreply.github.com>
2020-12-03 18:23:48 +0100

    remove redundant test

--- test_ai.py
+++ test_ai.py
@@ -7,25 +7,6 @@ from game import EMPTY, X, O
 
 
 class TestAI:
-    @mock.patch('random.choice')
-    @pytest.mark.skip()
-    def test_choose_action_random(self, choice_mock):
-        choice_mock.side_effect = self.get_first_arg
-
-        board = [
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]
-        ]
-
-        action = AI().choose_action(board)
-        call_args, _ = choice_mock.call_args
-
-        assert action == call_args[0][0]
-
-    def get_first_arg(self, *args):
-        return args[0][0]
-
     def test_choose_action(self):
         board = [
             [O, O, X],
